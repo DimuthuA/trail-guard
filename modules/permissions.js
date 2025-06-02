2535c902004293c68ed22ece04a1bc03eeaf6f2f
DimuthuA <dimuthu.h.amaraweera@gmail.com>
2025-06-02 17:49:28 +0530

    Add Foreground Service tags to AndroidManifest & modify permission request flow

--- modules/permissions.js
+++ modules/permissions.js
@@ -1,8 +1,10 @@
+import * as Application from 'expo-application';
+import * as IntentLauncher from 'expo-intent-launcher';
 import * as Location from 'expo-location';
-import { Alert } from 'react-native';
+import { Alert, Linking, Platform } from 'react-native';
 
 export async function requestLocationPermissions() {
-  // Request foreground location permission
+  // Request foreground location
   const { status: fgStatus } = await Location.requestForegroundPermissionsAsync();
 
   if (fgStatus !== 'granted') {
@@ -10,15 +12,37 @@ export async function requestLocationPermissions() {
     return false;
   }
 
-  // Then request background location permission
-  const { status: bgStatus } = await Location.requestBackgroundPermissionsAsync();
+  // Request background location (Android only)
+  if (Platform.OS === 'android') {
+    const { status: bgStatus } = await Location.requestBackgroundPermissionsAsync();
 
-  if (bgStatus !== 'granted') {
-    Alert.alert(
-      'Enable background location manually',
-      'Please go to Settings > Apps > Trail Guard > Permissions > Location and choose "Allow all the time".'
-    );
-    return false;
+    if (bgStatus !== 'granted') {
+      Alert.alert(
+        'Background location needed',
+        'To enable background tracking, allow "All the time" location access from app settings.',
+        [
+          {
+            text: 'Cancel',
+            style: 'cancel',
+          },
+          {
+            text: 'Open Settings',
+            onPress: () => {
+              if (Platform.Version >= 30) {
+                // Android 11+
+                IntentLauncher.startActivityAsync(
+                  IntentLauncher.ActivityAction.APPLICATION_DETAILS_SETTINGS,
+                  { data: `package:${Application.applicationId}` }
+                );
+              } else {
+                Linking.openSettings();
+              }
+            },
+          },
+        ]
+      );
+      return false;
+    }
   }
 
   return true;
